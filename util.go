95dda90d0fedf03026ee90673f45200b0d312711
sujit <s.baniya.np@gmail.com>
2025-03-16 08:26:06 +0545

    feat: Add other functions

--- util.go
+++ util.go
@@ -2,6 +2,7 @@ package json
 
 import (
 	"runtime"
+	"strconv"
 
 	"github.com/goccy/go-reflect"
 )
@@ -13,4 +14,20 @@ func FunctionPath(fn any) string {
 		return funcInfo.Name()
 	}
 	return ""
+}
+
+// A Number represents a JSON number literal.
+type Number string
+
+// String returns the literal text of the number.
+func (n Number) String() string { return string(n) }
+
+// Float64 returns the number as a float64.
+func (n Number) Float64() (float64, error) {
+	return strconv.ParseFloat(string(n), 64)
+}
+
+// Int64 returns the number as an int64.
+func (n Number) Int64() (int64, error) {
+	return strconv.ParseInt(string(n), 10, 64)
 }
